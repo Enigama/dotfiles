9e0c92c5cca658e6364b1824ae0b2700c2999d81
ykryshtal <ykryshtal@magnite.com>
2023-02-04 16:05:33 +0200

    feat(new config): remove old configs, add a new onec

--- config.fish
+++ config.fish
@@ -1,68 +1,127 @@
-#fisher add tuvistavie/fish-ssh-agent 
-# fisher add oh-my-fish/theme-bobthefish
-
-set -g -x fish_greeting 'Guten tag Enigama'
+set -g -x fish_greeting 'Guten tag Enigama, you become better every day!!!'
 set PAATH XDG_CONFIG_HOME $HOME /usr/bin/fish
 set -gx EDITOR nvim
 
 ##npm
 function ni
-  npm i
+    npm i
 end
 
-function nrw
-  npm run watch
+function nci
+    npm ci
+end
+
+function ys
+    yarn start
+end
+
+function yt
+    yarn test $argv
 end
 ##
 ##GIT ALIAS
 function gs
- git status
+    git status
 end
 
 function ga
- git add .
+    git add .
 end
 
 alias gcm='git commit -m'
 
 function gp
- git push
+    git push $argv
 end
 
 function gpr
-  git pull --rebase
+    git pull --rebase
 end
 
 function gcb
-  git checkout $argv
+    git checkout $argv
 end
 
 function gb
-  git branch
+    git branch
 end
 
 function gcf
-  git add .
-  git commit -m '[FRONT] fix'
-  git push
+    git add .
+    git commit -m 'fix'
+    git push
+end
+
+function gwl
+    git worktree list
+end
+
+function gwr
+    git worktree remove $argv
+end
+
+function gwa
+    git worktree add $argv
 end
 ##
+
+function V
+    vim .
+end
 
 #Confiuration files fast
 function cvim
-  nvim-qt -- -- ~/.config/nvim/init.vim
+    nvim ~/.config/nvim/init.vim
 end
 
 function cfish
-  nvim-qt -- -- ~/.config/fish/config.fish
+    nvim ~/.config/fish/config.fish
+end
+
+function cneofetch
+    nvim ~/.config/neofetch/config.conf
 end
 
 function ci3
-  nvim-qt -- -- ~/.i3/config
+    nvim ~/.config/i3/config
 end
 
-##Server Connect##
-function server
-  ssh seadora_ek@95.216.186.136
+function ctmux
+    nvim ~/.tmux.conf
 end
-##
+
+function cdac
+    nvim ~/Keyboard/Dactyl/config/dac_man_5x6.keymap
+end
+
+#function fish_greeting
+#  neofetch
+#end
+
+# Todo make work session first instead of fly
+if [ -z "$TMUX" ]
+    tmux new-session -d -s work -c ~/Projects/application-console.git
+    tmux attach fly || tmux new -s fly
+end
+
+#Tmux short
+function ta
+    if count $argv[1] >/dev/null
+        tmux a -t $argv[1]
+    else
+        tmux a -t work
+    end
+end
+
+#File manager by terminal
+function rd
+    ranger ~/Documents/
+end
+
+function stopServer
+    if count $argv[1] >/dev/null
+        sudo kill -9 (sudo lsof -t -i:$argv[1])
+    else
+        sudo kill -9 (sudo lsof -t -i:3000)
+    end
+end
