98529c015db70bc28855fc836f15b60343db2269
Enigama <enigama3@gmail.com>
2025-04-13 20:06:58 +0300

    ignore, nvim updates

--- nvim/.config/nvim/lua/enigama/git-figutive.lua
+++ nvim/.config/nvim/lua/enigama/git-figutive.lua
@@ -6,7 +6,12 @@ function M.config()
 	local keymap = vim.keymap.set
 	local opts = { noremap = true, silent = true }
 
-	keymap("n", "<leader>gs", ":G <cr>", opts)
+	keymap("n", "<leader>gs", function()
+		require("miss").has_missed_files(function()
+			vim.cmd(":G")
+		end)
+	end, opts)
+	-- keymap("n", "<leader>gs", ":G <cr>", opts)
 	keymap("n", "<leader>gl", ":GcLog <CR>", opts)
 end
 return M
--- nvim/.config/nvim/lua/enigama/octo.lua
+++ nvim/.config/nvim/lua/enigama/octo.lua
@@ -14,6 +14,35 @@ function M.config()
 		suppress_missing_scope = {
 			projects_v2 = true,
 		},
+		commands = {
+			pr = {
+				auto = function()
+					local gh = require("octo.gh")
+					local picker = require("octo.picker")
+					local utils = require("octo.utils")
+
+					local buffer = utils.get_current_buffer()
+
+					local auto_merge = function(number)
+						local cb = function()
+							utils.info("This PR will be auto-merged")
+						end
+						local opts = { cb = cb }
+						gh.pr.merge({ number, auto = true, squash = true, opts = opts })
+					end
+
+					if not buffer or not buffer:isPullRequest() then
+						picker.prs({
+							cb = function(selected)
+								auto_merge(selected.obj.number)
+							end,
+						})
+					elseif buffer:isPullRequest() then
+						auto_merge(buffer.node.number)
+					end
+				end,
+			},
+		},
 	})
 end
 
